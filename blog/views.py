3a3716524e8ea49bcdcea40cab224c996663c49d
BubeStefanoski <bubestefanoski8@gmail.com>
2021-08-25 21:22:28 +0200

    Initial Commit
    First time working with django

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return HttpResponse("<h1> Blog Home </h1>")
+
+def about(request):
+    return HttpResponse("<h1> About Site </h1>")
